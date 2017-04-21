f7a9313ad86715d7d1712366a05d8359c65e0420
Kirk Roerig <mr.possoms@gmail.com>
2017-04-21 11:49:49 -0400

    Initial Commit

---
+++ Proton Dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proton Dev
+//
+//  Created by Kirk Roerig on 4/21/17.
+//  Copyright © 2017 Kirk Roerig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
